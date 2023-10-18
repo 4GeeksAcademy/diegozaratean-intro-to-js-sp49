f3dfa41abf2cda2a934c036b158f6834b6fd3e75
diego <diegozaratean@gmail.com>
2023-10-18 13:37:42 +0000

    finshi dom class

--- main.js
+++ main.js
@@ -1,1 +1,41 @@
-console.log('desde main js')
+console.log('desde main js 123')
+
+
+const persona = {
+    nombre: 'pascual',
+    apellido: 'villa',
+    edad: 39,
+    skills: ['js','python','react'],
+    programar: function(){
+        console.log('estoy programando')
+    } 
+}
+
+
+const arrayejemplo = ['pascual','villa',39]
+
+
+
+// Objeto Perro nombre, raza, ladrar 'Wow'
+let perro = {
+    nombre: 'gines',
+    raza: 'chiguaua',
+    ladrar: function(){
+        console.log('Wow')
+    }
+
+}
+
+
+document.querySelectorAll('.text')[1].textContent = 'text 4'
+
+
+function changeTitle(){
+    console.log('changeTitle')
+    document.getElementById('title').innerText = 'Titulo desde botón'
+    document.getElementById('title').style.backgroundColor = 'blue'
+
+
+}
+
+// changeTitle()
