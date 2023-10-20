30a382ea6fb733bf601a994b5954874c16e9c7e0
diego <diegozaratean@gmail.com>
2023-10-20 13:18:29 +0000

    finish dice class

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ const persona = {
     apellido: 'villa',
     edad: 39,
     skills: ['js','python','react'],
+    'lugar de nacimiento': 'Madrid',
     programar: function(){
         console.log('estoy programando')
     } 
@@ -32,10 +33,35 @@ document.querySelectorAll('.text')[1].textContent = 'text 4'
 
 function changeTitle(){
     console.log('changeTitle')
-    document.getElementById('title').innerText = 'Titulo desde botón'
-    document.getElementById('title').style.backgroundColor = 'blue'
+    const names = ['Mario','Luigi','Toad','Bowser','Peach']
+    const colors = ['red','green','blue','violet']
+    
+    let nombre = names[Math.floor(Math.random() * names.length)]
+    let color = colors[Math.floor(Math.random() * colors.length)]
+    // let color = getRandomColor();
+    console.log(colorsgetRandomColor())
+    document.getElementById('title').innerText = 'Hola ' + nombre
+    document.getElementById('title').style.backgroundColor = color
 
 
 }
 
 // changeTitle()
+
+
+
+// Pseudo codigoo algorimot
+// DONE: crear la funcion lanzarDados()
+function lanzarDados(){
+    console.log('lanzarDados')
+    console.log(Math.ceil(Math.random() * 6))
+    console.log(Math.ceil(Math.random() * 6))
+    document.getElementById('dado1').innerHTML = Math.ceil(Math.random() * 6)
+    document.getElementById('dado2').innerHTML = Math.ceil(Math.random() * 6)
+}
+// lanzarDados()
+// DONE:  generar dos numeros aleatorio del 1 al 6
+// console.log(Math.ceil(Math.random() * 6))
+// console.log(Math.ceil(Math.random() * 6))
+// Mostrar el resultado en el html, vinvular js con html y con cada dado 
+// DONE: vincular html con js con el boton, cuando le de click al boton ejecutar una funcion
